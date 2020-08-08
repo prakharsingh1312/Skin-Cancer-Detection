de40b7a602d56833cf7a790f9bceca380fb56921
Prakhar Singh <prakharsingh13@gmail.com>
2020-08-09 01:06:06 +0530

    testing

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask ,  render_template
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Base App Running"
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("test.html")
 if __name__ == "__main__":
     app.run()
