6fa6770ef71d405ff9cfec3321dd11af3b62ec48
Prakhar Singh <prakharsingh13@gmail.com>
2020-08-09 01:26:00 +0530

    fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
-from flask import Flask ,  render_template
+from flask import Flask ,  render_template, request , session , redirect , url_for,flash
+from functions import *
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("test.html")
+	if request.method == 'POST':
+		send=[];
+		send.append(float(request.form['mass']))
+		send.append(float(request.form['size']))
+		send.append(float(request.form['mratio']))
+		send.append(float(request.form['damage']))
+		send.append(float(request.form['exparea']))
+		send.append(float(request.form['dratio']))
+		return str(tumor_size(send))	
+
+	return render_template("test.html")
 if __name__ == "__main__":
     app.run()
