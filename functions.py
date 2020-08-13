477a6600ce91ac2e7a181fd6ba3bc94c551d9692
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2020-08-13 16:13:24 +0530

    penalty fix

--- functions.py
+++ functions.py
@@ -93,33 +93,8 @@ def tumor_size(tester):
 	train = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Train.csv'))
 	test = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Test.csv'))
 	sample = pd.read_csv(os.path.join(os.path.dirname(__file__), 'sample_submission.csv'))
-	X = train.drop(['tumor_size','std_dev_malign', 'err_malign', 'malign_penalty'],axis=1)
+	X = train.drop(['tumor_size'],axis=1)
 	y = train['tumor_size'].copy()
-
-	tester.append(tester[3]/tester[5])
-	tester.append(tester[0]/tester[4])
-	tester.append(tester[5]/tester[3])
-
-	#Feature_Engineering
-	X['NF'] = X['damage_size']/X['damage_ratio']
-	test['NF'] = test['damage_size']/test['damage_ratio']
-
-	X['NF2'] = X['mass_npea']/X['exposed_area']
-	test['NF2'] = test['mass_npea']/test['exposed_area']
-
-	X['NF6'] = X['damage_ratio']/X['damage_size']
-	test['NF6'] = test['damage_ratio']/test['damage_size']
-
-
-	'''scores = []
-				#Regressor Training
-				for tr_in, val_in in KFold(random_state=48).split(X, y):
-				    X_train, y_train, X_val, y_val = X.iloc[tr_in], y[tr_in], X.iloc[val_in], y[val_in]
-				    model = ExtraTreesRegressor(n_jobs=-1,n_estimators=200)
-				    model.fit(X_train, y_train)
-				    scores.append(rmse(y_val,model.predict(X_val)))
-				print(np.mean(scores))'''
-
 
 	model = ExtraTreesRegressor(n_jobs=-1,n_estimators=200)
 
